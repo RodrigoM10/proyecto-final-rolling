81941b4f4a3c250f0a15e638a882ed5279fe8119
Rodrigo <rodrigomendozar10@gmail.com>
2021-10-01 12:23:07 -0300

    creacion de formato de carpetas y rutas en app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
+import { Switch, Route } from "react-router-dom";
 
 function App() {
+
   return (
     <div>
-      HOLA
+      {/* <Header /> */}
+          <Switch>
+              <Route path="/" exact>
+                {/* PAGE 1 */}
+              </Route>
+              <Route path="/" exact>
+                {/* PAGE 2 */}
+              </Route>
+              <Route path="/" exact>
+                {/* PAGE 3 */}
+              </Route>
+              <Route path="/" exact>
+                {/* PAGE 4 */}
+              </Route>
+          </Switch>
     </div>
   );
 }
