bbb35628008d20beeca6e5fa4ee12267e3bfdafe
NicoMen <nicomendoza.92@gmail.com>
2021-10-05 18:46:03 +0200

    se prueba rama

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,43 +1,28 @@
 import React from 'react'
-import { Button, Card, Col, Container, Form, InputGroup, Row } from 'react-bootstrap'
+import "./stylesPages/login.css";
+import { Button, Card, Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap'
 
 function Login() {
     return (
         <Container>
-        <Row>
-            <Col xs={12} sm={8} md={6} className="mx-auto my-5">
-                <Card className="border">
-                    <Card.Header className="bg-info">
-                        <h4 className="text-white">MeMes</h4>
-                    </Card.Header>
-                    <Card.Body>
-                        <Form noValidate>
-                            <Form.Group controlId="validationCustom02">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    name="email"
-                                    required
-                                    type="text"
-                                    placeholder="Last name"
-                                />
-                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group controlId="validationCustomUsername">
-                                <Form.Label>Password</Form.Label>
-                                <InputGroup hasValidation>
-                                    <Form.Control
-                                        minLength="6"
-                                        name="password"
-                                        type="password"
-                                        placeholder="****"
-                                        aria-describedby="inputGroupPrepend"
-                                        required
-                                    />
-                                    <Form.Control.Feedback type="invalid">s
-                                        Password is required!
-                                    </Form.Control.Feedback>
-                                </InputGroup>
-                            </Form.Group>
+            <Row>
+                <Col xs={12} sm={8} md={6} className="mx-auto my-5">
+                    <Card className="d-flex justify-content-center form-login ">
+                        <Card.Body>
+                            <div className="d-flex flex-column align-items-center">
+                            <h3 className="header">Welcome to Wine Vibes</h3>
+                            <p className="subHeader">Only for Members</p>
+                            </div>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Email address *"
+                                className="mb-3"
+                            >
+                                <Form.Control type="email" placeholder="name@example.com" />
+                            </FloatingLabel>
+                            <FloatingLabel controlId="floatingPassword" label="Password">
+                                <Form.Control type="password" placeholder="Password *" />
+                            </FloatingLabel>
                             <Row>
                                 <Button type="submit" className="mx-auto mt-4">
                                     Iniciar Sesión
@@ -48,12 +33,14 @@ function Login() {
                                     Cerrar Sesion
                                 </Button>
                             </Row>
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
-    </Container>
+                            <Row>
+                               ¿Aun No tienes cuenta? 
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container >
     )
 }
 
