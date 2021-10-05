3edb907750f46c81de7b2bc59ad7a5fa1d6eeeff
NicoMen <nicomendoza.92@gmail.com>
2021-10-05 19:05:49 +0200

    se realiza estructura de login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -6,38 +6,57 @@ function Login() {
     return (
         <Container>
             <Row>
-                <Col xs={12} sm={8} md={6} className="mx-auto my-5">
-                    <Card className="d-flex justify-content-center form-login ">
+                <Col className="col-12 col-lg-6 d-flex flex-column justify-content-between mx-auto my-5">
+                    <Card className="d-flex justify-content-center form-login">
                         <Card.Body>
                             <div className="d-flex flex-column align-items-center">
-                            <h3 className="header">Welcome to Wine Vibes</h3>
-                            <p className="subHeader">Only for Members</p>
+                                <h3 className="header">Welcome to Wine Vibes</h3>
+                                <p className="subHeader">Only for Members</p>
                             </div>
                             <FloatingLabel
                                 controlId="floatingInput"
-                                label="Email address *"
+                                label="Email address"
                                 className="mb-3"
                             >
                                 <Form.Control type="email" placeholder="name@example.com" />
                             </FloatingLabel>
                             <FloatingLabel controlId="floatingPassword" label="Password">
-                                <Form.Control type="password" placeholder="Password *" />
+                                <Form.Control type="password" placeholder="Password" />
                             </FloatingLabel>
                             <Row>
                                 <Button type="submit" className="mx-auto mt-4">
                                     Iniciar Sesión
                                 </Button>
-                            </Row>
-                            <Row>
-                                <Button className="mx-auto btn-secondary mt-4">
-                                    Cerrar Sesion
+                                <Button className="mx-auto btn mt-4">
+                                    Inicia Sesion con Facebook
                                 </Button>
-                            </Row>
-                            <Row>
-                               ¿Aun No tienes cuenta? 
+                                <div>
+                                    <p>¿Aun no tienes cuenta?</p>
+                                    <a href=".">Crea una cuenta</a>
+                                </div>
                             </Row>
                         </Card.Body>
                     </Card>
+                </Col>
+                <Col className="col-12 col-lg-4 d-flex flex-column justify-content-between mx-auto my-5 ">
+                    <Card>
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
                 </Col>
             </Row>
         </Container >
