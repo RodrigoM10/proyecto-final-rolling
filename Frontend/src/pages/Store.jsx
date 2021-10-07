fcea1b411428c3fb630827218190d9df0856d9cc
Aljotar <jotaralvaro@gmail.com>
2021-10-07 20:40:42 -0300

    Primer commit

--- Frontend/src/pages/Store.jsx
+++ Frontend/src/pages/Store.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Col, Row, Container } from "react-bootstrap";
+import CardProductos from "../components/CardProductos";
 
 function Store() {
-    return (
-        <div>
-            Store
-        </div>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>
+          <CardProductos />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Store
+export default Store;
