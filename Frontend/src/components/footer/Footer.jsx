3ee1899bcb87667bb0c3541986b5d894c5c34201
Rodrigo <rodrigomendozar10@gmail.com>
2021-10-07 16:34:01 -0300

    footer checkeado

--- Frontend/src/components/footer/Footer.jsx
+++ Frontend/src/components/footer/Footer.jsx
@@ -1,54 +1,63 @@
 import React from 'react'
 import "./footer.css";
 
+// react-icons
+import { FaArrowUp, FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaYoutubeSquare } from 'react-icons/fa';
+
 export const Footer = () => {
     return (
-        <div>
+        <div className="mt-auto">
             <div className="footer">
                 <div className="row text-center align-items-center">
                     <div className="col-12  my-4">
-                        <div className="mb-2">
+                        <div className="mb-2 social-icon">
                             <h5>SEGUINOS EN NUESTRAS REDES</h5>
-                            <a href="." target="blank"><i
-                                className="fab fa-facebook-square social-icon"></i></a>
-                            <a href="." target="blank"><i
-                                className="fab fa-instagram-square social-icon"></i></a>
-                            <a href="." target="blank"><i
-                                className="fab fa-twitter-square social-icon"></i></a>
+                            <a href="." target="blank">
+                                <FaFacebookSquare  />
+                            </a>
+                            <a href="." target="blank">
+                                <FaTwitterSquare />
+                            </a>
+                            <a href="." target="blank">
+                                <FaInstagramSquare />
+                            </a>
+                            <a href="." target="blank">
+                                <FaYoutubeSquare />
+                            </a>
                         </div>
                     </div>
                 </div>
                 <hr />
-                <div className="row text-center align-items-center">
+                <div className="row text-center align-items-center link-effect">
                     <div className="col-12 col-lg-3 col-md-6 my-4">
                         <h5>CONCATENOS</h5>
                         <div className="links mb-2">
-                            <a href="." className="link-effect">contactwine@winevibes.com</a><br />
-                            <a href="." target="blank" className="link-effect">+3816807710</a>
+                            <a href="." >contactwine@winevibes.com</a><br />
+                            <a href="." >+3816807710</a>
                         </div>
                     </div>
                     <div className="col-12 col-lg-3 col-md-6 my-4">
                         <h5>BODEGAS</h5>
                         <div className="links mb-2">
-                            <a href="." target="blank" className="link-effect" >Teo Calderon</a><br />
-                            <a href="." target="blank" className="link-effect" >Aventura</a><br />
-                            <a href="." target="blank" className="link-effect" >Don omar</a>
+                            <a href="."  >Teo Calderon</a><br />
+                            <a href="."  >Aventura</a><br />
+                            <a href="."  >Don omar</a>
                         </div>
                     </div>
                     <div className="col-12 col-lg-3 col-md-6 my-4">
                         <h5>EVENTOS</h5>
                         <div className="links">
-                            <a href="." target="blank" className="link-effect" >La fiesta del Secreto</a><br />
-                            <a href="." target="blank" className="link-effect" >Los Pecados Capitales</a><br />
-                            <a href="." target="blank" className="link-effect" >Finca La Escondida</a>
+                            <a href="."  >La fiesta del Secreto</a><br />
+                            <a href="."  >Los Pecados Capitales</a><br />
+                            <a href="."  >Finca La Escondida</a>
                         </div>
                     </div>
                     <div className="col-12 col-lg-3 col-md-6 my-4">
                         <h5>SOPORTE</h5>
                         <div className="links">
-                            <a href="." target="blank" className="link-effect" >Servicio Tecnico</a><br />
-                            <a href="." target="blank" className="link-effect" >Avisos Legales</a><br />
-                            <a href="." target="blank" className="link-effect" >Cookies</a>
+                            <a href="."  >Servicio Tecnico</a><br />
+                            <a href="."  >Avisos Legales</a><br />
+                            <a href="."  >Cookies</a>
                         </div>
                     </div>
                 </div>
@@ -61,7 +70,7 @@ export const Footer = () => {
                     <p>Keep Calm and drink Wine</p>
                 </div>
                 <div>
-                    <a href="." target="blank" className="volverArriba"><i class="fas fa-arrow-up"></i></a>
+                    <a href="." target="blank" className="volverArriba"><FaArrowUp /></a>
                 </div>
             </div>
         </div>
