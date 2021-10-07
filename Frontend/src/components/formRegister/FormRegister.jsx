beef5bb12089bc7755df7ab3e549eb84c4cc0bc1
Rodrigo <rodrigomendozar10@gmail.com>
2021-10-07 18:11:14 -0300

    maquetado de form register

--- Frontend/src/components/formRegister/FormRegister.jsx
+++ Frontend/src/components/formRegister/FormRegister.jsx
@@ -4,34 +4,35 @@ import './formRegister.css'
 
 export const FormRegister = () => {
     return (
-        <Form className="form-register my-5">
-            <Form.Group className="mb-3 row align-items-center" controlId="formBasicEmail">
-                <label className="col-3">Nombre </label>
-                <input className="col-9" type="text" />
+        <Form className="form-register my-5  ">
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+                <label className="col-11 col-md-3">Nombre </label>
+                <input className="col-11 col-md-9" type="text" />
             </Form.Group>
-            <Form.Group className="mb-3 row align-items-center" controlId="formBasicEmail">
-                <label className="col-3 align-items-center">Apellido</label>
-                <input className="col-9" type="text" />
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+                <label className="col-11 col-md-3 align-items-center">Apellido</label>
+                <input className="col-11 col-md-9" type="text" />
             </Form.Group>
-            <Form.Group className="mb-3 row align-items-center" controlId="formBasicEmail">
-                <label className="col-3 align-items-center">Email</label>
-                <input className="col-9" type="email" />
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+                <label className="col-11 col-md-3 align-items-center">Email</label>
+                <input className="col-11 col-md-9" type="email" />
             </Form.Group>
-            <Form.Group className="mb-3 row align-items-center" controlId="formBasicEmail">
-                <label className="col-3">Contraseña</label>
-                <input className="col-9" type="password" />
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+                <label className="col-11 col-md-3">Contraseña</label>
+                <input className="col-11 col-md-9" type="password" />
             </Form.Group>
-            <Form.Group className="mb-3 row align-items-center" controlId="formBasicEmail">
-                <label className="col-3" >Nacimiento</label>
-                <div className="col-9 row-cols-3 p-0">
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+                <label className="col-11 col-md-3" >Nacimiento</label>
+                <div className="col-11 col-md-9 row-cols-3 p-0">
                     <input type="number" placeHolder="DD" id="birth-day" min="1" max="31" maxLength="2" required />
                     <input type="number" placeHolder="MM" id="birth-day" min="1" max="12" maxLength="2" required />
                     <input type="number" placeHolder="AAAA" id="birth-day" min="1" max="31" minLength="4" required />
                 </div>
             </Form.Group>
             <hr />
-            <Form.Group className="mb-4 d-flex flex-column align-items-start justify-content-center" controlId="formBasicEmail">
-                <label className="mb-2 "> ¿Que estas buscando de nuestra bodega?</label>
+            <div className="d-flex flex-column align-items-center align-items-md-start justify-content-center">
+            <Form.Group className="mb-4 d-flex flex-column align-items-center align-items-md-start justify-content-center" controlId="formBasicEmail">
+                <label className="mb-2 text-center"> ¿Que estas buscando de nuestra bodega?</label>
                 <select name="" id="">
                     <option>Por favor elija una opcion</option>
                     <option value="1">Vinos premiun</option>
@@ -43,6 +44,7 @@ export const FormRegister = () => {
                 <Form.Check type="checkbox" label="Manténgame informado sobre mis pedidos, estado de entrega, noticias y ofertas exclusivas de miembros" />
             </Form.Group>
             <button href="/login" exact className="responsive-navbar-button">Iniciar sesión</button>
+            </div>
         </Form>
     )
 }
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -7,13 +7,14 @@ function Register() {
         <div className="container-register">
         <Container >
             <section className="row  row-cols-1 row-cols-lg-2">
-                <div className="text-welcome-register">
+                <div className="text-welcome-register d-flex flex-column aling-items-center align-items-md-start ">
                     <h1>Bienvenido a Rolling Wine</h1>
-                    <p>Únete gratis a The Wine Collective y sé el primero en enterarte de nuestras mejores ofertas y nuevos lanzamientos. Obtendrá acceso exclusivo a las mejores ofertas de vinos de Argentina, prioridad para comprar vinos raros y de lanzamiento limitado, competiciones y visitas al bodegon únicas.</p>
+                    <p className="text-center text-md-start px-5 px-md-0 pe-md-5">Únete gratis a The Wine Collective y sé el primero en enterarte de nuestras mejores ofertas y nuevos lanzamientos. Obtendrá acceso exclusivo a las mejores ofertas de vinos de Argentina, prioridad para comprar vinos raros y de lanzamiento limitado, competiciones y visitas al bodegon únicas.</p>
+                    <h5 className="text-center text-md-start px-5 px-md-0 pe-md-5 ">REGÍSTRESE AHORA GRATIS y sea recompensado por cada compra a través de nuestro Programa de Recompensas de Vino.</h5>
                 </div>
-                <div>
+                <Container>
                   <FormRegister />
-                </div>
+                </Container>
             </section>
         </Container>
         </div>
