23b53be325b996ab8765455db811b96c4596f5ce
NicoMen <nicomendoza.92@gmail.com>
2021-10-08 17:44:22 +0200

    se agrego estilos en login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import CardsLogin from '../components/cardsLogin/CardsLogin';
+import { CardsLogin } from '../components/cardsLogin/CardsLogin';
 import { FormLogin } from '../components/formLogin/FormLogin';
 
 function Login() {
@@ -8,10 +8,10 @@ function Login() {
     return (
         <Container>
             <Row>
-                <Col className="col-12 col-lg-6 d-flex flex-column justify-content-between mx-auto my-5">
+                <Col className="col-12 col-lg-8 d-flex flex-column justify-content-between mx-auto my-5">
                     <FormLogin />
                 </Col>
-                <Col className="col-12 col-lg-4 d-flex flex-column justify-content-between mx-auto my-5 ">
+                <Col className="col-12 col-lg-4 mx-auto my-5">
                     <CardsLogin />
                 </Col>
             </Row>
