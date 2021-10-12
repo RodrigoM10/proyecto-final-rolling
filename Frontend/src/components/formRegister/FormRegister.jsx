ae61f47de8df960e1f162246fcb52aaa6feff87d
Rodrigo <rodrigomendozar10@gmail.com>
2021-10-12 10:03:32 -0300

    formRegister

--- Frontend/src/components/formRegister/FormRegister.jsx
+++ Frontend/src/components/formRegister/FormRegister.jsx
@@ -21,27 +21,27 @@ export const FormRegister = () => {
                 <label className="col-11 col-md-3">Contraseña</label>
                 <input className="col-11 col-md-9" type="password" />
             </Form.Group>
-            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+            {/* <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
                 <label className="col-11 col-md-3">Nacimiento</label>
                 <input className="col-11 col-md-9 text-center" type="date" />
-            </Form.Group>
-            {/* <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
+            </Form.Group> */}
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicEmail">
                 <label className="col-11 col-md-3" >Nacimiento</label>
                 <div className="col-11 col-md-9 row-cols-3 p-0">
                     <input type="number" placeHolder="DD" id="birth-day" min="1" max="31" maxLength="2" required />
                     <input type="number" placeHolder="MM" id="birth-day" min="1" max="12" maxLength="2" required />
                     <input type="number" placeHolder="AAAA" id="birth-day" min="1" max="31" minLength="4" required />
                 </div>
-            </Form.Group> */}
+            </Form.Group>
             <hr />
             <div className="d-flex flex-column align-items-center align-items-md-start justify-content-center">
             <Form.Group className="mb-4 d-flex flex-column align-items-center align-items-md-start justify-content-center" controlId="formBasicEmail">
                 <label className="mb-2 text-center"> ¿Que estas buscando de nuestra bodega?</label>
-                <select name="" id="">
+                <select type="text"  id="">
                     <option>Por favor elija una opcion</option>
-                    <option value="1">Vinos premiun</option>
-                    <option value="2">Visitas al bodegon</option>
-                    <option value="3">Nuevas Ofertas</option>
+                    <option type="text" value="1">Vinos premiun</option>
+                    <option type="text" value="2">Visitas al bodegon</option>
+                    <option type="text" value="3">Nuevas Ofertas</option>
                 </select>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
