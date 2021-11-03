ba026237438370e16119016d3558e5ffcb094a31
NicoMen <nicomendoza.92@gmail.com>
2021-11-03 18:45:04 +0100

    se puso condicional filtro gral

--- Frontend/src/components/cardProduct/CardsProduct.jsx
+++ Frontend/src/components/cardProduct/CardsProduct.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
 import { CardProduct } from "./CardProduct";
 import './cardProduct.css'
 
 
-export const CardsProduct = ({allProducts, favorites, setFavorites, cart, setCart, setShowSideCart }) => {
+export const CardsProduct = ({ allProducts, favorites, setFavorites, cart, setCart, setShowSideCart }) => {
   const mapProductos = allProducts?.map((producto, i) => (
     <CardProduct key={i} producto={producto}
       favorites={favorites}
@@ -14,7 +15,12 @@ export const CardsProduct = ({allProducts, favorites, setFavorites, cart, setCar
   ));
   return (
     <>
-      <div className="d-flex flex-wrap justify-content-center">{mapProductos}</div>
+      {allProducts.length === 0 ?
+        <Card className="no-results-card text-center text-dark-50 p-5 m-5">
+          <Card.Title>Producto no encontrado</Card.Title>
+        </Card> :
+        <div className="d-flex flex-wrap justify-content-center">{mapProductos}</div>
+      }
     </>
   )
 }
