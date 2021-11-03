876957c8f7f275a2dd5aeba3ac7146fa2ad98bdd
Rodrigo <rodrigomendozar10@gmail.com>
2021-11-03 18:38:26 -0300

    productsPaginated

--- Frontend/src/components/cardProduct/CardsProduct.jsx
+++ Frontend/src/components/cardProduct/CardsProduct.jsx
@@ -4,7 +4,18 @@ import './cardProduct.css'
 
 
 export const CardsProduct = ({allProducts, favorites, setFavorites, cart, setCart, setShowSideCart }) => {
-  const mapProductos = allProducts?.map((producto, i) => (
+
+  const limit = 9;
+  console.log("🚀~ file: CardsProduct.jsx ~ line 9 ~ CardsProduct ~ limit", limit)
+  const initial = 1 ;
+  console.log("🚀 ~ file: CardsProduct.jsx ~ line 11 ~ CardsProduct ~ initial", initial)
+  const last = initial + limit;
+  console.log("🚀 ~ file: CardsProduct.jsx ~ line 13 ~ CardsProduct ~ last", last)
+  const productsPaginated = allProducts.slice(initial, last);
+  console.log("🚀 ~ file: CardsProduct.jsx ~ line 16 ~ CardsProduct ~ productsPaginated", productsPaginated)
+
+
+  const mapProductos = productsPaginated?.map((producto, i) => (
     <CardProduct key={i} producto={producto}
       favorites={favorites}
       setFavorites={setFavorites}
